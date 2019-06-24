b41af4b398966437329b9e46c500c7fc2b621971
Garrison <joshua.garrison@gmail.com>
2019-06-24 14:37:09 -0400

    Help topic repository, fix user and ticket creation,

---
+++ OSTicketAPI.NET/Helpers/OstThreadEntryExtensions.cs
@@ -0,0 +1,42 @@
+﻿using System.Linq;
+using Microsoft.EntityFrameworkCore.Internal;
+using Newtonsoft.Json.Linq;
+using OSTicketAPI.NET.Entities;
+
+namespace OSTicketAPI.NET.Helpers
+{
+    public static class OstThreadEntryExtensions
+    {
+        public static string ToFriendlyString(this OstThreadEvent threadEvent)
+        {
+            var data = JObject.Parse(threadEvent.Data);
+
+            if (!data.HasValues)
+                return "Unknown";
+
+            switch (data.First.Path)
+            {
+                case "status":
+                    var status = data["status"][1];
+                    return $"Status changed to \"{status}\"";
+                case "source":
+                    var source = data["source"][1];
+                    return $"Ticket source changed to \"{source}\"";
+                case "claim":
+                    return $"Ticket has been claimed by {threadEvent.Username}";
+                case "add":
+                    var add = data["add"].First.First.Children().ToList();
+                    var add1 = add[0].Children().ToList().First();
+                    return $"Added \"{add1}\" as a ticket coordinator";
+                case "del":
+                    var del = data["del"].First.First.Children().ToList();
+                    var del1 = del[0].Children().ToList().First();
+                    return $"Removed \"{del1}\" as a ticket coordinator";
+                case "topic_id":
+                    return $" {threadEvent.Username} changed the topic of the ticket";
+                default:
+                    return "Unknown";
+            }
+        }
+    }
+}
---
+++ OSTicketAPI.NET/Interfaces/IHelpTopicsRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using OSTicketAPI.NET.Entities;
+
+namespace OSTicketAPI.NET.Interfaces
+{
+    public interface IHelpTopicsRepository
+    {
+        Task<IEnumerable<OstHelpTopic>> GetHelpTopics(bool onlyPublicTopic = true);
+        Task<OstHelpTopic> GetHelpTopicsByTopicId(int topicId, bool onlyPublicTopic = true);
+        Task<IEnumerable<OstHelpTopic>> GetHelpTopicsByDepartmentId(int departmentId, bool onlyPublicTopic = true);
+    }
+}
