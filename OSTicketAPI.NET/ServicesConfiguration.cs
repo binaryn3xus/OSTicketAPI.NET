7af0e77a9c4a902e1b285d106465b2022fc24f9b
Joshua Garrison <joshuagarrison27@gmail.com>
2019-09-06 10:00:47 -0400

    Add testing for DI for services

--- OSTicketAPI.NET/ServicesConfiguration.cs
+++ OSTicketAPI.NET/ServicesConfiguration.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Extensions.Configuration;
+﻿using System;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using OSTicketAPI.NET.DTO;
 using OSTicketAPI.NET.Logging;
@@ -20,16 +21,21 @@ namespace OSTicketAPI.NET
 
             if (options != null)
             {
-                logger.Info("Attempting to load OSTicket settings using {OptionsType}", options.GetType().Name);
+                logger?.Info("Attempting to load OSTicket settings using {OptionsType}", options.GetType().Name);
                 services.AddSingleton(new OSTicketService(options.ConnectionString, new OSTicketOfficialApi(options.BaseUrl, options.ApiKey)));
             }
-            else
+            else if (configuration != null)
             {
-                logger.Info("Attempting to load OSTicket settings using {OptionsType}", configuration.GetType().Name);
+                logger?.Info("Attempting to load OSTicket settings using {OptionsType}", configuration.GetType().Name);
                 var connectionString = configuration.GetValue<string>("OSTicket:DatabaseConnectionString");
                 var baseUrl = configuration.GetValue<string>("OSTicket:BaseUrl");
                 var apiKey = configuration.GetValue<string>("OSTicket:ApiKey");
                 services.AddSingleton(new OSTicketService(connectionString, new OSTicketOfficialApi(baseUrl, apiKey)));
+            }
+            else
+            {
+                logger?.Error("Unable to set up OSTicketService - No IConfiguration setup OR OSTicketServicesOptions setup.");
+                throw new Exception("Unable to set up OSTicketService - No IConfiguration setup OR OSTicketServicesOptions setup.");
             }
         }
     }
