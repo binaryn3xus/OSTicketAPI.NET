6ec676907353ae1ee69703c54fd4077cd442cf03
Joshua Garrison <joshuagarrison27@gmail.com>
2019-09-13 11:25:38 -0400

    Adding ListRepository features

---
+++ OSTicketAPI.NET/Interfaces/IListRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace OSTicketAPI.NET.Interfaces
+{
+    public interface IListRepository<T>
+    {
+        Task<IEnumerable<T>> GetLists(Expression<Func<T, bool>> expression = null);
+    }
+}
