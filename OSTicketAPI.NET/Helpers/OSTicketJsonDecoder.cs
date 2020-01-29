2f797e3de2df835b7f2dae249563ed58d5c40880
Joshua Garrison <joshuagarrison27@gmail.com>
2020-01-29 12:18:42 -0500

    Change name back and check for null for Thread Event data

--- OSTicketAPI.NET/Helpers/OSTicketJsonDecoder.cs
+++ OSTicketAPI.NET/Helpers/OSTicketJsonDecoder.cs
@@ -1,5 +1,4 @@
 ﻿using System.Linq;
-using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 using OSTicketAPI.NET.Entities;
 
@@ -7,42 +6,38 @@ namespace OSTicketAPI.NET.Helpers
 {
     public static class OSTicketJsonDecoder
     {
-        public static string DataJsonToString(this OstThreadEvent threadEvent)
+        public static string ToFriendlyString(this OstThreadEvent threadEvent)
         {
-            try
-            {
-                var data = JObject.Parse(threadEvent?.Data);
+            if (string.IsNullOrWhiteSpace(threadEvent?.Data))
+                return null;
 
-                if (!data.HasValues)
-                    return "Unknown";
+            var data = JObject.Parse(threadEvent.Data);
 
-                switch (data.First.Path)
-                {
-                    case "status":
-                        var status = data["status"][1];
-                        return $"Status changed to \"{status}\"";
-                    case "source":
-                        var source = data["source"][1];
-                        return $"Ticket source changed to \"{source}\"";
-                    case "claim":
-                        return $"Ticket has been claimed by {threadEvent?.Username}";
-                    case "add":
-                        var add = data["add"].First.First.Children().ToList();
-                        var add1 = add[0].Children().First();
-                        return $"Added \"{add1}\" as a ticket coordinator";
-                    case "del":
-                        var del = data["del"].First.First.Children().ToList();
-                        var del1 = del[0].Children().First();
-                        return $"Removed \"{del1}\" as a ticket coordinator";
-                    case "topic_id":
-                        return $" {threadEvent?.Username} changed the topic of the ticket";
-                    default:
-                        return "Unknown";
-                }
-            }
-            catch (JsonReaderException)
-            {
+            if (!data.HasValues)
                 return "Unknown";
+
+            switch (data.First.Path)
+            {
+                case "status":
+                    var status = data["status"][1];
+                    return $"Status changed to \"{status}\"";
+                case "source":
+                    var source = data["source"][1];
+                    return $"Ticket source changed to \"{source}\"";
+                case "claim":
+                    return $"Ticket has been claimed by {threadEvent.Username}";
+                case "add":
+                    var add = data["add"].First.First.Children().ToList();
+                    var add1 = add[0].Children().First();
+                    return $"Added \"{add1}\" as a ticket coordinator";
+                case "del":
+                    var del = data["del"].First.First.Children().ToList();
+                    var del1 = del[0].Children().First();
+                    return $"Removed \"{del1}\" as a ticket coordinator";
+                case "topic_id":
+                    return $" {threadEvent.Username} changed the topic of the ticket";
+                default:
+                    return "Unknown";
             }
         }
     }
